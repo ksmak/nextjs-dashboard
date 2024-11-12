81e1ddf31d6db106323370bcedd676a9749a863e
Smakov Kairat <ksmakov@gmail.com>
2024-11-12 16:05:58 +0500

    add form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -54,7 +54,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-    } catch (error) {
+    } catch {
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
@@ -78,7 +78,7 @@ export async function updateInvoice(id: string, formData: FormData) {
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `;
-    } catch (error) {
+    } catch {
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
@@ -92,7 +92,7 @@ export async function deleteInvoice(id: string) {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
-    } catch (error) {
+    } catch {
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }
 }
